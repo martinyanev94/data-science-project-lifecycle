05fd4d7e89dae9a3a864f7fff9c398df927cea24
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-30 04:22:43 -0400

    Add Lesson_8_Model_Evaluation_And_Tuning_Strategies/part_4_roc_curve_plot.bash

---
+++ Lesson_8_Model_Evaluation_And_Tuning_Strategies/part_4_roc_curve_plot.bash
@@ -0,0 +1,19 @@
+pip install matplotlib
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+# Calculate probabilities and ROC curve
+y_prob = model.predict_proba(X_test)[:, 1]
+fpr, tpr, _ = roc_curve(y_test, y_prob)
+roc_auc = auc(fpr, tpr)
+
+# Plot ROC curve
+plt.plot(fpr, tpr, color='blue', label=f'ROC curve (area = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='red', linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
