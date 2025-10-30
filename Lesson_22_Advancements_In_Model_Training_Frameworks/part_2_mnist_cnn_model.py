628b68206dfaf5064aaf67514a71d779b61596e1
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-30 04:20:31 -0400

    Add Lesson_22_Advancements_In_Model_Training_Frameworks/part_2_mnist_cnn_model.py

---
+++ Lesson_22_Advancements_In_Model_Training_Frameworks/part_2_mnist_cnn_model.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Load and preprocess the MNIST dataset
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+X_train = X_train.reshape((60000, 28, 28, 1)).astype('float32') / 255
+X_test = X_test.reshape((10000, 28, 28, 1)).astype('float32') / 255
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+# Build the model
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=5, batch_size=64, validation_data=(X_test, y_test))
